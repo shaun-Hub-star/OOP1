8ac46136e83c4cc9e65e57657ec2ba7865ef1047
Your Name <zloofe@post.bgu.ac.il>
2021-04-25 23:43:03 +0300

    kekw

--- Grade100/src/PolyMath/Polynomial.java
+++ Grade100/src/PolyMath/Polynomial.java
@@ -5,6 +5,18 @@ import java.util.List;
 
 public class Polynomial {
     private ArrayList<Monomial> poly;
+    public static  Polynomial build(String input){
+        String[] monomialList = input.split("\\s+");
+        //iterates on the list of strings
+        for(int i = 0; i < monomialList.length;i++){
+            int k = 0;
+            //evaluates the string's value in a certain possition in the array
+            if(monomialList[i].charAt(0) == )
+            for(int l = 0; l < monomialList[i].length(); l++){
+                k = k* 10 + monomialList[i].charAt(l);
+            }
+        }
+    }
     public Polynomial add(){
         return null;
     }
@@ -18,7 +30,7 @@ public class Polynomial {
         return null;
     }
     public String toString(){
-        
+
     }
 
 
