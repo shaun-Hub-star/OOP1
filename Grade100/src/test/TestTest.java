09d56ffa15ea11fc453660f7e7afc33011931ee1
Your Name <zloofe@post.bgu.ac.il>
2021-04-27 20:31:40 +0300

    added testing package

---
+++ Grade100/src/test/TestTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import PolyMath.Polynomial;
+import PolyMath.Rational;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+
+    // testing integer
+    @BeforeAll
+    public void setUp(){
+        PolyMath.Integer integer = new PolyMath.Integer(5);
+        PolyMath.Integer integer2 = new PolyMath.Integer(5);
+        PolyMath.Integer integer3 = new PolyMath.Integer(5);
+    }
+
+    @Test
+
+    public int colculate(){
+
+    }
+
+}
